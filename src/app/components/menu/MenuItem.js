c471e4ff21648ded4ff02e096318822691a35375
vankeith-sys <vkalmazan@gmail.com>
2025-09-17 20:56:08 +0800

    another fix

--- src/app/components/menu/MenuItem.js
+++ src/app/components/menu/MenuItem.js
@@ -3,7 +3,7 @@ import {CartContext} from "../AppContext";
 import MenuItemTile from "./MenuItemTile";
 import Image from "next/image";
 import {useContext, useState} from "react";
-import FlyingButton from "react-flying-item";
+import FlyingButton from "../CustomFlyingButton";
 import toast from "react-hot-toast";
 
 export default function MenuItem(menuItem) {
