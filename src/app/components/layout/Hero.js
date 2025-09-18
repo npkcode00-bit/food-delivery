3347b0d84b0b04de511e33b6b6216ac717d9ba60
vankeith-sys <vkalmazan@gmail.com>
2025-09-18 08:03:01 +0800

    lint fix

--- src/app/components/layout/Hero.js
+++ src/app/components/layout/Hero.js
@@ -1,39 +1,48 @@
 import Right from "../icons/Right";
 
+export default function Hero() {
+  return (
+    <section className="hero mt-4">
+      <div className="py-4">
+        <h1 className="text-4xl font-semibold">
+          Everything <br />
+          is better <br />
+          with a&nbsp;
+          <span className="text-primary">Pizza</span>
+        </h1>
 
-export default function Hero () {
-    return (
-        <section className="hero mt-4">
-            <div className="py-4">
-                 <h1 className="text-4xl font-semibold">
-                    Everything <br/> 
-                    is better <br/> 
-                    with a &nbsp;
-                   <span className="text-primary">
-                    Pizza
-                    </span>
-                </h1>
+        <p className="my-6 text-gray-500 text-sm">
+          <q>
+            Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
+            consectetur, adipisci velit...
+          </q>{" "}
+          <q>
+            There is no one who loves pain itself, who seeks after it and wants
+            to have it, simply because it is pain...
+          </q>
+        </p>
 
-                <p className="my-6 text-gray-500 text-sm">"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
-                "There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..." </p>
-                
-                <div className="flex gap-4 text-sm ">
-                    <button style={{color:'white',display:'flex'}} className="flex  bg-primary uppercase items-center gap-2  px-8 py-2 rounded-full "> 
-                        Order now 
-                        <Right />
-                    </button>
-                    <button style={{display:'flex', border:'none'}}  className="flex items-center border-0 gap-2 py-2 text-gray-600 font-semibold"> 
-                        Learn more
-                        <Right />    
-                    </button>
-                </div>
-            </div>
-        
-        
-            <div className="relative">
-                 <img src={'/pizza.png'} alt={'hero'} />
-            </div>
-        
-        </section>
-    )
+        <div className="flex gap-4 text-sm">
+          <button
+            type="button"
+            className="flex items-center gap-2 px-8 py-2 rounded-full bg-primary text-white uppercase"
+          >
+            Order now
+            <Right />
+          </button>
+          <button
+            type="button"
+            className="flex items-center gap-2 py-2 font-semibold text-gray-600"
+          >
+            Learn more
+            <Right />
+          </button>
+        </div>
+      </div>
+
+      <div className="relative">
+        <img src="/pizza.png" alt="Slice of pizza" />
+      </div>
+    </section>
+  );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,58 +1,49 @@
- import Header from "./components/layout/Header";
+import Header from "./components/layout/Header";
 import Hero from "./components/layout/Hero";
 import HomeMenu from "./components/layout/HomeMenu";
 import SectionHeaders from "./components/layout/SectionHeaders";
 
-
 export default function Home() {
   return (
-  <>
-   <Hero />
-   <HomeMenu />
-   <section className="text-center my-16">
-    <SectionHeaders subHeader={'Our stoy'} mainHeader={'About us'} />
+    <>
+      <Hero />
+      <HomeMenu />
 
-    <div className="text-gray-500 max-w-md mx-auto mt-4 flex flex-col gap-4">
-      <p>
-        It is a long established fact that a reader will be distracted by 
-        the readable content of a page when looking at its layout. The point
-        of using Lorem Ipsum is that it has a more-or-less normal distribution
-          of letters, as opposed to using 'Content here, content here', making it
-          look like readable English. Many desktop publishing packages and web page 
-          editors now use Lorem Ipsum as their default model text, and a search for 'lor
-      </p>
+      <section className="text-center my-16">
+        <SectionHeaders subHeader={"Our story"} mainHeader={"About us"} />
 
-      <p>
-        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots 
-        in a piece of classical Latin literature from 45 BC, making it over 2000 years old. 
-        Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia,
-        looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum
-          
-      </p>
+        <div className="text-gray-500 max-w-md mx-auto mt-4 flex flex-col gap-4">
+          <p>
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page when looking at its layout. The point
+            of using Lorem Ipsum is that it has a more-or-less normal distribution
+            of letters, as opposed to using &apos;Content here, content here&apos;,
+            making it look like readable English.
+          </p>
 
-       <p>
-        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots 
-        in a piece of classical Latin literature from 45 BC, making it over 2000 years old. 
-        
-      </p>
+          <p>
+            Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots
+            in a piece of classical Latin literature from 45 BC, making it over 2000 years old.
+            Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia,
+            looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum.
+          </p>
 
-    </div>
+          <p>
+            Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots
+            in a piece of classical Latin literature from 45 BC, making it over 2000 years old.
+          </p>
+        </div>
+      </section>
 
+      <section className="text-center my-8">
+        <SectionHeaders subHeader={"Don't hesitate"} mainHeader={"Contact us"} />
 
-   </section>
-
-   <section className="text-center my-8">
-    <SectionHeaders subHeader={'Don\'t hesitate'} mainHeader={'Contact us'} />
-
-    <div className="mt-8">
-        <a className="text-4xl underline text-gray-500" href="tel:+09312341232">
-          <h1>+09 762 2324</h1>
-        </a>
-    </div>
-  
-    
-   </section>
-
-  </>
+        <div className="mt-8">
+          <a className="text-4xl underline text-gray-500" href="tel:+09312341232">
+            <h1>+09 762 2324</h1>
+          </a>
+        </div>
+      </section>
+    </>
   );
 }
